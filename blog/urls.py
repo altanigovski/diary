d540e932a6a6e01bd8fa3b60994da8760a2536be
alien <11219@protonmail.ch>
2020-05-21 13:48:57 +0200

    smaller font, Fondation, about page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    url(r'^entry/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now())\
-		.order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now())\
+	    .order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def about(request):
+    return render(request, 'blog/about.html')
